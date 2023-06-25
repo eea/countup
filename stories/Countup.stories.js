356cfb482a6a3d7c89adc13acf47a505abdce2d9
Dobricean Ioan Dorian <dobriceanionut1408@gmail.com>
2023-06-25 17:01:20 +0300

    stories

---
+++ stories/Countup.stories.js
@@ -0,0 +1,18 @@
+import Countup from "../src/Countup";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+export default {
+  title: "Example/Countup",
+  component: Countup,
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary = {
+  args: {
+    end: 15,
+    duration: 12,
+    decimals: 1,
+    prefix: "",
+    suffix: "EURO",
+  },
+};
