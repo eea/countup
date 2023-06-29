c4d49bb50221e02feb172866767b238bb525967f
Dobricean Ioan Dorian <dobriceanionut1408@gmail.com>
2023-06-29 14:01:16 +0300

    add razzle

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@
 module.exports = function (api) {
   api.cache(true);
 
-  const presets = ['@babel/preset-env', '@babel/preset-react'];
+  const presets = ['@babel/preset-env', '@babel/preset-react', 'razzle'];
   const plugins = ['macros'];
 
   return {
