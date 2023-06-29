e8cf955b3edd89f2417202bee529eb84ed0a1ccd
Dobricean Ioan Dorian <dobriceanionut1408@gmail.com>
2023-06-29 12:53:18 +0300

    check unit tests

--- src/tests/Countup.test.jsx
+++ src/tests/Countup.test.jsx
@@ -1,5 +1,3 @@
-import Countup from '../components/Countup.jsx';
-
 describe('Filter function', () => {
-  test('it should filter by a search term (link)', () => {});
+  test('doar vreau sa testez pe jenkins', () => {});
 });
