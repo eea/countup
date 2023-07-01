0b66fce007bc1461da395764fc6be427b6b5bde1
Dobricean Ioan Dorian <dobriceanionut1408@gmail.com>
2023-07-01 13:07:37 +0300

    plugins

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,23 @@ module.exports = function (api) {
   api.cache(true);
 
   const presets = ['@babel/preset-env', '@babel/preset-react', 'razzle'];
-  const plugins = ['macros'];
+  const plugins = [
+    'macros',
+    [
+      '@babel/plugin-transform-spread',
+      {
+        loose: true,
+      },
+    ],
+    [
+      '@babel/plugin-proposal-class-properties',
+      {
+        loose: true,
+      },
+    ],
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-proposal-export-default-from',
+  ];
 
   return {
     presets,
