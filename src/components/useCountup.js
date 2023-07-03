5f7439ab53393fc663a7cc7a4041641ce9c102e4
Dobricean Ioan Dorian <dobriceanionut1408@gmail.com>
2023-07-03 21:02:50 +0300

    hook

---
+++ src/components/useCountup.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+
+function useCountup() {
+  const [counter, setCounter] = useState(0);
+  const [paused, setPaused] = useState(0);
+
+  const pauseResume = () => {
+    setPaused(!paused);
+  };
+  const start = () => {
+    setPaused(false);
+  };
+  const reset = () => {
+    setPaused(true);
+    setCounter(0);
+  };
+
+  return {
+    pauseResume,
+    start,
+    reset,
+    Countup: ({
+      end,
+      duration,
+      decimals,
+      prefix = '',
+      suffix = '',
+      children = <></>,
+    }) => {
+      useEffect(() => {
+        let start = 0;
+        let increment = 1;
+        if (decimals > 0) {
+          increment /= Math.pow(10, decimals);
+        }
+        if (Math.abs(start - end) <= increment / 10) return;
+
+        let totalMilSecDur = parseInt(duration);
+        let incrementTime =
+          (totalMilSecDur / Math.abs(end * Math.pow(10, decimals || 0))) * 1000;
+        let timer = setInterval(() => {
+          if (start < end) start += increment;
+          else start -= increment;
+          setCounter(start.toFixed(decimals));
+          if (Math.abs(start - end) <= increment / 10) clearInterval(timer);
+        }, incrementTime);
+        return () => clearInterval(timer);
+      }, [duration, end, decimals]);
+      return (
+        <div>
+          {prefix}
+          {counter}
+          {suffix} {children}
+        </div>
+      );
+    },
+  };
+}
+
+export default useCountup;
